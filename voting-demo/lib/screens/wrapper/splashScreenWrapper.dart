00965abc463d976b042266b3760684a98aaec329
winfred <winfred242002@gmail.com>
2022-08-23 11:17:06 +0000

    :computer:setting up

---
+++ voting-demo/lib/screens/wrapper/splashScreenWrapper.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:easy_splash_screen/easy_splash_screen.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+import 'mainWrapper.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return EasySplashScreen(
+      logo: Image.asset('assets/splash.png',),
+      title:  Text(
+        "ELECT",
+        style: TextStyle(
+          fontSize: 67,
+          fontWeight: FontWeight.bold,
+          color: HexColor('#732424'),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      showLoader: true,
+      loadingText: Text(
+        "Loading...",
+        style: TextStyle(
+            color: HexColor('#732424')
+        ),
+      ),
+      navigator: const MainWrapper(),
+      durationInSeconds: 3,
+    );;
+  }
+}
