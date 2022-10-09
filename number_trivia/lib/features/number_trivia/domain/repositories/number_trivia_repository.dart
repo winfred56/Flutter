bdb18619a4947d82048dcc3a1b5190c13d5d239e
winfred <winfred242002@gmail.com>
2022-10-09 01:33:37 +0000

    :100: abstract class

---
+++ number_trivia/lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+
+
+import '../../../../core/error/Failures.dart';
+import '../entities/number_trivia.dart';
+
+abstract class NumberTriviaRepository{
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
+}
