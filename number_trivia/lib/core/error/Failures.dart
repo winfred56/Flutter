871f8cf210ad33a4cede22e2cf5e1487c7dcb55d
winfred <winfred242002@gmail.com>
2022-10-11 03:49:15 +0000

    :100: refactor

--- number_trivia/lib/core/error/Failures.dart
+++ number_trivia/lib/core/error/Failures.dart
@@ -3,5 +3,5 @@ import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable{
 
-  Failure([List properties = const <dynamic>[]]) : super(properties);
+  const Failure([List properties = const <dynamic>[]]) : super();
 }
