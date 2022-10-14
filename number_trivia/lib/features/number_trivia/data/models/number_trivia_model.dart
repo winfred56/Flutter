123b64e3b3d9b257705de414bc190190850da4da
winfred <winfred242002@gmail.com>
2022-10-14 00:35:57 +0000

    :zap: number_trivia_model

---
+++ number_trivia/lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,5 @@
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia{
+  NumberTriviaModel({required super.text, required super.number});
+}
