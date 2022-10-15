3db4e5efb8d2bbfb9e1fc3674808168dac87ebe2
winfred <winfred242002@gmail.com>
2022-10-15 10:53:14 +0000

    :alien: Campaign Screen

--- dribble_design1/lib/pages/campaign.dart
+++ dribble_design1/lib/pages/campaign.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class Campaign extends StatefulWidget {
   const Campaign({Key? key}) : super(key: key);
@@ -10,7 +12,91 @@ class Campaign extends StatefulWidget {
 class _CampaignState extends State<Campaign> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: HexColor('#7f8080'),
+      appBar: AppBar(
+        elevation: 0,
+        automaticallyImplyLeading: true,
+        backgroundColor: HexColor('#7f8080'),
+        title: Align(
+          alignment: Alignment.topRight,
+          child: CircleAvatar(
+            radius: 25,
+            backgroundColor: Colors.white,
+            backgroundImage: const NetworkImage(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAY6rHUk-UzP-qHRIW3EBWLYLN4wG53DvpKA&usqp=CAU"),
+            child: Stack(
+              children: const [
+                Positioned(
+                  top: 5,
+                  right: 0.1,
+                  child: CircleAvatar(
+                    radius: 5,
+                    backgroundColor: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: SafeArea(
+        minimum: const EdgeInsets.symmetric(horizontal: 10),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              /// Svg Picture
+              SvgPicture.asset(
+                "assets/svgs/modelling.svg",
+                height: MediaQuery.of(context).size.height * 0.6,
+              ),
+
+              /// title text
+              const Text(
+                  "Start your\nCampaign now",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+
+              /// Subtitle text
+              const Padding(
+                padding: EdgeInsets.only(top: 15.0),
+                child: Text(
+                    "No hidden changes no stealth fees",
+                  style: TextStyle(
+                    color: Colors.white70
+                  )
+
+                ),
+              ),
+
+              const Padding(padding: EdgeInsets.only(top: 50),),
+
+              /// create campaign button
+              SizedBox(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.06,
+                child: ElevatedButton(
+                    onPressed: (){},
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.black,),
+                  ),
+                    child: const Text(
+                        "Create Campaign",
+                      style: TextStyle(
+                        color: Colors.white
+                      )
+                    ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
 
