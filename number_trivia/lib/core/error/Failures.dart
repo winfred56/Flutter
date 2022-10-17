4982b7c2e9a29f253ceb8805be1d048abb9fbaff
winfred <winfred242002@gmail.com>
2022-10-17 02:01:18 +0000

    :red_heart: exceptions

--- number_trivia/lib/core/error/Failures.dart
+++ number_trivia/lib/core/error/Failures.dart
@@ -3,9 +3,14 @@ import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable{
 
-  //final List properties = const <dynamic>[];
+  final List properties = const <dynamic>[];
+
   const Failure([List properties = const <dynamic>[]]) : super();
 
-  // @override
-  // List get props => [properties];
+  @override
+  List get props => [properties];
 }
+
+/// General Failures
+class ServerException extends Failure {}
+class CacheException extends Failure {}
