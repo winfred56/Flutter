0e1ff03499f600efa0a46f1241b2a07934d7f51e
winfred <winfred242002@gmail.com>
2022-10-18 00:49:48 +0000

    :fire: generate mocks

--- weather_app/test/features/current_weather/domain/use_cases/get_current_weather_tests.dart
+++ weather_app/test/features/current_weather/domain/use_cases/get_current_weather_tests.dart
@@ -3,9 +3,11 @@ import 'package:mockito/mockito.dart';
 import 'package:mockito/annotations.dart';
 import 'package:weather_app/features/current_weather/domain/repositories/weather_repository.dart';
 
+import 'get_current_weather_tests.mocks.dart';
+
 
 @GenerateMocks([WeatherRepository])
 
 void main() {
-
+  MockWeatherRepository mockWeatherRepository = MockWeatherRepository();
 }
