aacd1cc4f6db177527508e8216d680b0f11a64c3
winfred <winfred242002@gmail.com>
2022-10-19 01:19:14 +0000

    :blue_heart: test for vaild return model

---
+++ weather_app/test/features/current_weather/data/models/weather_model_test.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:weather_app/features/current_weather/data/models/weather_model.dart';
+import 'package:weather_app/features/current_weather/domain/entities/weather.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+
+void main() {
+  var tWeatherModel =  const WeatherModel(temp: 28, humidity: 67);
+  
+  test('Should be a subclass of Weather entity', () {
+    /// assert
+    expect(tWeatherModel, isA<Weather>());
+  });
+  
+  group("from Json", () {
+    test('Should return a valid weather model', () async {
+      /// arrange
+      final Map<String, dynamic> jsonMap = json.decode(fixture('fixtures.json'));
+
+      /// act
+      final result = WeatherModel.fromJson(jsonMap);
+      
+      /// assert
+      expect(result, tWeatherModel);
+    });
+  });
+}
