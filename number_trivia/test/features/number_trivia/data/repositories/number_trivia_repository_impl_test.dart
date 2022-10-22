fbac886a00abb64a82deb1d0212bb97f59c33f69
winfred <winfred242002@gmail.com>
2022-10-22 01:07:54 +0000

    generate mocks

--- number_trivia/test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
+++ number_trivia/test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -1,26 +1,24 @@
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:number_trivia/core/platform/network_info.dart';
 import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:number_trivia/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'number_trivia_repository_impl_test.mocks.dart';
 
-class MockNetworkInfo extends Mock implements NetworkInfo {}
-
-class MockLocalDataSource extends Mock implements NumberTriviaLocalDataSource {}
-
-class MockRemoteDataSource extends Mock implements NumberTriviaRemoteDataSource{}
+@GenerateMocks([NetworkInfo, NumberTriviaRemoteDataSource, NumberTriviaLocalDataSource])
 
 void main() {
-  NumberTriviaRepositoryImpl repository;
-  MockRemoteDataSource mockRemoteDataSource;
-  MockLocalDataSource mockLocalDataSource;
-  MockNetworkInfo  mockNetworkInfo;
+  late NumberTriviaRepositoryImpl repository;
+  late MockNumberTriviaRemoteDataSource mockRemoteDataSource;
+  late MockNumberTriviaLocalDataSource mockLocalDataSource;
+  late MockNetworkInfo  mockNetworkInfo;
 
   setUp(() {
-    mockRemoteDataSource = MockRemoteDataSource();
+    mockRemoteDataSource = MockNumberTriviaRemoteDataSource();
     mockNetworkInfo = MockNetworkInfo();
-    mockLocalDataSource = MockLocalDataSource();
+    mockLocalDataSource = MockNumberTriviaLocalDataSource();
     repository = NumberTriviaRepositoryImpl(
       remoteDataSource: mockRemoteDataSource,
       localDataSource: mockLocalDataSource,
