4868f3743a8d4f9cdfd300ec0f750cce4d6af357
winfred <winfred242002@gmail.com>
2022-10-23 07:15:44 +0000

    😇 settings_page

--- dribble_design1/lib/shared/routes/routes.dart
+++ dribble_design1/lib/shared/routes/routes.dart
@@ -2,7 +2,9 @@ import 'package:dribble_design1/pages/account.dart';
 import 'package:flutter/cupertino.dart';
 
 import '../../pages/campaign.dart';
+import '../../pages/edit_personal_info_page.dart';
 import '../../pages/notifications.dart';
+import '../../pages/settings_page.dart';
 import '../../pages/sign_in.dart';
 import '../../pages/social_media_profile_page.dart';
 
@@ -15,8 +17,12 @@ class Routes{
   static const campaign = '/campaign';
   /// Open [Accountpage]
   static const account = '/account';
-
+  /// Open [SocialMediaProfilePage]
   static const socialMediaProfile = '/social-media-profile';
+  /// Open [SettingsPage]
+  static const settings = '/settings';
+  /// Open [EditPersonalInfo]
+  static const editPersonalInfo = '/edit-personal-info';
 
   static final Map<String, WidgetBuilder> routes = {
     Routes.signUp : (BuildContext context) => const SignIn(),
@@ -24,6 +30,8 @@ class Routes{
     Routes.campaign : (BuildContext context) => const Campaign(),
     Routes.socialMediaProfile: (BuildContext context) => const SocialMediaProfilePage(),
     Routes.account : (BuildContext context) => const Account(),
+    Routes.settings : (BuildContext context) => const SettingsPage(),
+    Routes.editPersonalInfo : (BuildContext context) => const EditPersonalInfo(),
   };
 
 }
