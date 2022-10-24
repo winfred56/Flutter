78fa6c3482536f735447413cd4f161354c17b1a0
winfred <winfred242002@gmail.com>
2022-10-24 22:10:54 +0000

    :zap:access remote api

--- number_trivia/lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ number_trivia/lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -1,4 +1,5 @@
 import '../models/number_trivia_model.dart';
+import 'package:http/http.dart';
 
 abstract class NumberTriviaRemoteDataSource{
   /// calls the https://numbersapi.com/{number} endpoint.
@@ -11,4 +12,22 @@ abstract class NumberTriviaRemoteDataSource{
   ///
   /// Throws a [ServerException] for all error codes.
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  final http.Client client;
+  NumberTriviaRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) {
+    // TODO: implement getConcreteNumberTrivia
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() {
+    // TODO: implement getRandomNumberTrivia
+    throw UnimplementedError();
+  }
+  
 }
