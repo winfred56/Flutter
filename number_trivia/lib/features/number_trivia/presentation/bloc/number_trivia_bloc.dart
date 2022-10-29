fccb4988ba10591a92afbcb90048edf788c9be44
winfred <winfred242002@gmail.com>
2022-10-29 18:09:20 +0000

    :hammer: bloc error fixed

--- number_trivia/lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ number_trivia/lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -33,7 +33,7 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
       final inputEither =
       inputConverter.stringToUnsignedInteger(event.numberString);
 
-      inputEither.fold((failure) async {
+      await inputEither.fold((failure) async {
         emit(const Error(message: INVALID_INPUT_FAILURE_MESSAGE));
       }, (integer) async {
         emit(Loading());
