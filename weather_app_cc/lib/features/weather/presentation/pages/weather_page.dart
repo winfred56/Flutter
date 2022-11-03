2e548474f5948eb09b4fcfb79e68b1f94e821e21
winfred <winfred242002@gmail.com>
2022-11-03 02:14:09 +0000

    :zap: change to stful

--- weather_app_cc/lib/features/weather/presentation/pages/weather_page.dart
+++ weather_app_cc/lib/features/weather/presentation/pages/weather_page.dart
@@ -5,8 +5,14 @@ import 'package:weather_app_cc/features/weather/presentation/bloc/weather_bloc.d
 
 import '../../../../injection_container.dart';
 
-class WeatherPage extends StatelessWidget {
+class WeatherPage extends StatefulWidget {
   const WeatherPage({Key? key}) : super(key: key);
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
 
   @override
   Widget build(BuildContext context) {
