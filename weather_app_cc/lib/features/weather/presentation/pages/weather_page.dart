e3a5e00d7e13ca15572734444b536d1acd4dc5e1
winfred <winfred242002@gmail.com>
2022-11-12 14:51:11 +0000

    :zap:

--- weather_app_cc/lib/features/weather/presentation/pages/weather_page.dart
+++ weather_app_cc/lib/features/weather/presentation/pages/weather_page.dart
@@ -84,7 +84,7 @@ class LoadingWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: MediaQuery.of(context).size.height / 3,
       child: const Center(
         child: CircularProgressIndicator(),
