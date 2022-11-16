2189371c4d686766ed919064309f87693e3b9d11
winfred <winfred242002@gmail.com>
2022-11-16 19:05:45 +0000

    :zap: call usecase

---
+++ untitled/lib/shared/usecases/usecase.dart
@@ -0,0 +1,32 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+import '../errors/failure.dart';
+
+abstract class Usecase<Type, Params>{
+  /// Construct to call
+  Future<Either<Failure, Type>> call(Params params);
+
+}
+
+/// Used when usecases doesn't take any parameters
+class NoParams extends Equatable{
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
+
+/// Used for token/string value required usecases
+class StringParams extends Equatable{
+  /// Constructor
+  const StringParams(this.value);
+
+  ///A single value parameter
+  final String value;
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [value];
+
+}
