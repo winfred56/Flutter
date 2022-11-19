3dd6f2e756b3bf516ba35b3760651ab434e71e41
winfred <winfred242002@gmail.com>
2022-11-19 07:42:02 +0000

    :fire: add device preview

--- untitled/lib/main.dart
+++ untitled/lib/main.dart
@@ -1,68 +1,38 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'features/phone/presentation/pages/phone_page.dart';
+import 'injection_container.dart' as di;
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+Future<void> main() async{
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (BuildContext context) => MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+      home: const PhonePage(),
+    ),)
+      );
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   // @override
+//   // Widget build(BuildContext context) {
+//   //   return MaterialApp(
+//   //     title: 'Flutter Demo',
+//   //     theme: ThemeData(
+//   //       primarySwatch: Colors.blue,
+//   //     ),
+//   //     home: const MyHomePage(title: 'Flutter Demo Home Page'),
+//   //   );
+//   // }
+// }
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
