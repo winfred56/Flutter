031683fd7896568c254f065a6d17d23a9c55d82e
winfred <winfred242002@gmail.com>
2022-11-21 22:20:27 +0000

    :zap: remove comments

--- untitled/lib/main.dart
+++ untitled/lib/main.dart
@@ -28,19 +28,5 @@ void main() async{
       );
 }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   // This widget is the root of your application.
-//   // @override
-//   // Widget build(BuildContext context) {
-//   //   return MaterialApp(
-//   //     title: 'Flutter Demo',
-//   //     theme: ThemeData(
-//   //       primarySwatch: Colors.blue,
-//   //     ),
-//   //     home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//   //   );
-//   // }
-// }
+
 
