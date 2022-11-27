304a64609115d0423aad4d114e27cd6d76272c70
winfred <winfred242002@gmail.com>
2022-11-27 04:27:18 +0000

    :zap: change default location to kumasi

--- google maps/lib/main.dart
+++ google maps/lib/main.dart
@@ -38,7 +38,7 @@ class MapSampleState extends State<MapSample> {
 
   static final CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
+      target: LatLng(6.691667, -1.662667),
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
