f5c273f3de59ded096c6e559371d15d9648d12f5
winfred <winfred242002@gmail.com>
2022-11-28 05:16:52 +0000

    :palm_tree: refactor

--- mini shop/lib/main.dart
+++ mini shop/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:device_preview/device_preview.dart';
-import 'package:mini_shop/src/authentication/presentation/pages/authenticate.dart';
+import 'package:mini_shop/src/authentication/presentation/widgets/login_page.dart';
 
 void main() => runApp(
   DevicePreview(
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       builder: DevicePreview.appBuilder,
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
-      home: const Authenticate(),
+      home: const LoginPage(),
     );
   }
 }
