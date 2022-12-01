e82d6576bda5474c0b80eb28921144e55d07821f
winfred <winfred242002@gmail.com>
2022-12-01 03:21:24 +0000

    :zap: sign up form

---
+++ mini shop/lib/src/authentication/presentation/widgets/signup_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        minimum: const EdgeInsets.symmetric(horizontal: 10),
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              //SvgPicture.asset('assets/svgs/1.svg'),
+              Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Email Address'),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: .7),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: .7),
+                        ),
+                      ),
+                      controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8),
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Password'),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: .7),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: .7),
+                        ),
+                      ),
+                      controller: passwordController,
+                      obscureText: true,
+                    ),
+                    const Padding(padding: EdgeInsets.symmetric(vertical: 8)),
+                    SizedBox(
+                      width: double.infinity,
+                      height: MediaQuery.of(context).size.height * 0.07,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Login'),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Don't have an account?"),
+                        TextButton(onPressed: ()=> const SignUpPage(), child: const Text('Sign Up'),)
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
