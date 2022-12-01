f8686907ef3201b49c577f491997911378bd5ed7
winfred <winfred242002@gmail.com>
2022-12-01 03:28:51 +0000

    :zap: create routes

---
+++ mini shop/lib/shared/routes/routes.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:mini_shop/src/authentication/presentation/widgets/login_page.dart';
+import 'package:mini_shop/src/authentication/presentation/widgets/signup_page.dart';
+
+class Routes{
+  /// Open [SignUpPage]
+  static const signUp = '/sign-up';
+  /// Open [LoginPage]
+  static const login = '/login';
+
+
+  static final Map<String, WidgetBuilder> routes = {
+    Routes.signUp : (BuildContext context) => const SignUpPage(),
+    Routes.login: (BuildContext context) => const LoginPage(),
+  };
+
+}
