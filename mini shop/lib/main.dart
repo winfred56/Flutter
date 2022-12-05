d7ef9cc3d7a30c65c025ae5c04c40a70bac3d27a
winfred <winfred242002@gmail.com>
2022-12-05 09:46:35 +0000

    :palm_tree: initialize firebase

--- mini shop/lib/main.dart
+++ mini shop/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
@@ -5,12 +6,16 @@ import 'package:device_preview/device_preview.dart';
 import 'package:mini_shop/shared/routes/routes.dart';
 import 'package:mini_shop/src/authentication/presentation/widgets/login_page.dart';
 
-void main() => runApp(
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
   DevicePreview(
     enabled: !kReleaseMode,
     builder: (context) => MyApp(), // Wrap your app
   ),
 );
+}
 
 class MyApp extends StatelessWidget {
   @override
