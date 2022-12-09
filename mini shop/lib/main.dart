70ce909fdd6273decfe61090cb23d807c8cb251f
winfred <winfred242002@gmail.com>
2022-12-09 02:47:23 +0000

    :zap: stream provider for auth changes

--- mini shop/lib/main.dart
+++ mini shop/lib/main.dart
@@ -1,32 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:device_preview/device_preview.dart';
 import 'package:mini_shop/shared/routes/routes.dart';
+import 'package:mini_shop/src/authentication/data/remote_datasource.dart';
 import 'package:mini_shop/src/authentication/presentation/widgets/login_page.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(
-  DevicePreview(
-    enabled: !kReleaseMode,
-    builder: (context) => MyApp(), // Wrap your app
-  ),
-);
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(), // Wrap your app
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      useInheritedMediaQuery: true,
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
-      theme: ThemeData.light(),
-      routes: Routes.routes,
-      home: const LoginPage(),
+    return StreamProvider<User?>.value(
+      value: AuthenticateUsers().getUser(),
+      initialData: null,
+      child: MaterialApp(
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        theme: ThemeData.light(),
+        routes: Routes.routes,
+        home: const LoginPage(),
+      ),
     );
   }
 }
