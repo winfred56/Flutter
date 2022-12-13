ca1e1bfeb8af116996eee010dd21f8db68c93cc4
winfred <winfred242002@gmail.com>
2022-12-13 03:07:55 +0000

    :electric_plug: action chips

--- mini shop/lib/features/products/presentation/pages/products_home_page.dart
+++ mini shop/lib/features/products/presentation/pages/products_home_page.dart
@@ -11,7 +11,65 @@ class _ProductsHomePageState extends State<ProductsHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar()
+      body: SafeArea(
+        minimum: const EdgeInsets.symmetric(horizontal: 10),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const CircleAvatar(
+                    radius: 30,
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.shopping_cart_rounded,
+                        size: 34,
+                      )),
+                ]),
+              const Padding(padding: EdgeInsets.only(top: 10)),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: ActionChip(label: const Text('Shoes 👟',), onPressed: () {}),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: ActionChip(label: const Text('Bags 👜',), onPressed: () {}),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: ActionChip(label: const Text('Shirts 👕',), onPressed: () {}),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: ActionChip(label: const Text('Jeans 👖',), onPressed: () {}),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: ActionChip(label: const Text('Beach 🏖',), onPressed: () {}),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: ActionChip(label: const Text('Accessories 🕶',), onPressed: () {}),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: ActionChip(label: const Text('Street wear 🧥',), onPressed: () {}),
+                    ),
+                  ],
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
