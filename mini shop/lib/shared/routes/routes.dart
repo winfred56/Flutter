0e4c48e323855267cfef8f550d4830086c09f473
winfred <winfred242002@gmail.com>
2022-12-14 22:45:49 +0000

    :palm_tree: item detail route

--- mini shop/lib/shared/routes/routes.dart
+++ mini shop/lib/shared/routes/routes.dart
@@ -3,6 +3,7 @@ import 'package:mini_shop/src/authentication/presentation/widgets/login_page.dar
 import 'package:mini_shop/src/authentication/presentation/widgets/signup_page.dart';
 
 import '../../features/products/presentation/pages/products_home_page.dart';
+import '../../features/products/presentation/widgets/product_detail.dart';
 
 class Routes{
   /// Open [SignUpPage]
@@ -11,12 +12,15 @@ class Routes{
   static const login = '/login';
   /// Open [ProductsPage]
   static const products = '/products';
+  /// Open [ProductsDetails]
+  static const productsDetails = '/products_details';
 
 
   static final Map<String, WidgetBuilder> routes = {
     Routes.signUp : (BuildContext context) => const SignUpPage(),
     Routes.login: (BuildContext context) => const LoginPage(),
-    Routes.products: (BuildContext context) => const ProductsHomePage()
+    Routes.products: (BuildContext context) => const ProductsHomePage(),
+    Routes.productsDetails: (BuildContext context) => const ProductDetail(),
   };
 
 }
