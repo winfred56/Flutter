4889cc702c87f78a9d9b84f770cf0c69a0d2caa1
winfred <winfred242002@gmail.com>
2022-12-16 02:22:59 +0000

    :zap: generate code

---
+++ mini shop/lib/src/profile/domain/entities/profile.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'profile.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Profile _$$_ProfileFromJson(Map<String, dynamic> json) => _$_Profile(
+      id: json['id'] as String,
+      phoneNumber: json['phoneNumber'] as String,
+      username: json['username'] as String,
+      email: json['email'] as String,
+      fullName: json['fullName'] as String,
+      photo: json['photo'] as String,
+    );
+
+Map<String, dynamic> _$$_ProfileToJson(_$_Profile instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'phoneNumber': instance.phoneNumber,
+      'username': instance.username,
+      'email': instance.email,
+      'fullName': instance.fullName,
+      'photo': instance.photo,
+    };
