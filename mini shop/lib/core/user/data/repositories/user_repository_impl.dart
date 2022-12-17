ac238b21bc5c822531bddecf493edc8d46386699
winfred <winfred242002@gmail.com>
2022-12-17 14:45:53 +0000

    :hammer: registration first try

--- mini shop/lib/core/user/data/repositories/user_repository_impl.dart
+++ mini shop/lib/core/user/data/repositories/user_repository_impl.dart
@@ -1,8 +1,5 @@
-
 import 'package:dartz/dartz.dart';
-
-import 'package:mini_shop/core/user/domain/entities/user.dart';
-
+import 'package:flutter/foundation.dart';
 import 'package:mini_shop/shared/error/failure.dart';
 
 import '../../../../shared/network/network.dart';
@@ -11,37 +8,59 @@ import '../datasources/user_remote_datasource.dart';
 
 class UserRepositoryImpl implements UserRepository {
   /// Constructor
-  UserRepositoryImpl(
-      {required this.networkInfo,
-        required this.remoteDatabase,
-        });
+  UserRepositoryImpl({
+    required this.networkInfo,
+    required this.remoteDatabase,
+  });
 
   /// Dependencies
   NetworkInfo networkInfo;
   UserRemoteDatasource remoteDatabase;
 
   @override
-  Future<Either<Failure, void>> logOut() {
-    // TODO: implement logOut
-    throw UnimplementedError();
+  Future<Either<Failure, void>> logOut() async {
+    try {
+      await networkInfo.hasInternet();
+      final result = await remoteDatabase.logOut();
+      return right(result);
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+        print('From User_repository_impl logOut block');
+      }
+      return left(const Failure('Error Logging Out'));
+    }
   }
 
   @override
-  Future<Either<Failure, void>> registerWithEmailAndPassword(String email, String password) async {
+  Future<Either<Failure, void>> registerWithEmailAndPassword(
+      String email, String password) async {
     try {
       await networkInfo.hasInternet();
       final result = await remoteDatabase.register(email, password);
       return right(result);
     } catch (e) {
-      print(e);
-      print('From User_repository_impl Register block');
+      if (kDebugMode) {
+        print(e);
+        print('From User_repository_impl Register block');
+      }
       return left(const Failure('Error Registering'));
     }
   }
 
   @override
-  Future<Either<Failure, void>> signInWithEmailAndPassword(String email, String password) {
-    // TODO: implement signInWithEmailAndPassword
-    throw UnimplementedError();
+  Future<Either<Failure, void>> signInWithEmailAndPassword(
+      String email, String password) async {
+    try{
+      await networkInfo.hasInternet();
+      final result = await remoteDatabase.signIn(email, password);
+      return right(result);
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+        print('From User_repository_impl Sign In block');
+      }
+      return left(const Failure('Error Logging In'));
+    }
   }
 }
