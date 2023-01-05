5d8fef6418be645a74d090adcecd9c1b0d551766
winfred <winfred242002@gmail.com>
2023-01-05 23:56:22 +0000

    :100:

--- dawurobo/lib/core/post/presentation/bloc/PostBloc.dart
+++ dawurobo/lib/core/post/presentation/bloc/PostBloc.dart
@@ -3,22 +3,34 @@ import 'package:dawurobo/core/post/domain/use_cases/retreive.dart';
 
 import '../../../../shared/usecase/usecase.dart';
 import '../../domain/entities/post.dart';
+import '../../domain/use_cases/retrieve_specific_post.dart';
 
-class PostBloc{
+class PostBloc {
   ///Constructor
-  PostBloc({required this.createPost, required this.retrievePosts});
+  PostBloc({required this.createPost, required this.retrievePosts, required this.specificPost});
 
   final CreatePost createPost;
   final RetrievePosts retrievePosts;
+  final GetSpecificPost specificPost;
 
-  /// Create Post
+
+
+
+  /// Create [Post]
   Future<String?> create(Post post) async {
     final result = await createPost(CreatePostParams(post));
     return result.fold((failure) => failure.message, (post) => null);
   }
 
+  /// Get all [Post]s
   Future<List<Post>> retrieve() async {
     final result = await retrievePosts(NoParams());
     return result.fold((failure) => [Post.initial()], (post) => post);
+  }
+
+  /// Get a specific [Post]
+  Future<Post> getSpecificPost(String documentId) async {
+    final result = await specificPost(StringParams(documentId));
+    return result.fold((failure) => Post.initial(), (post) => post);
   }
 }
--- dawurobo/lib/src/home/presentation/pages/home_page.dart
+++ dawurobo/lib/src/home/presentation/pages/home_page.dart
@@ -1,6 +1,15 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dawurobo/core/post/presentation/pages/create.dart';
+import 'package:dawurobo/core/post/presentation/pages/post_detail.dart';
+import 'package:dawurobo/src/device/domain/entities/device.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import '../../../../core/post/domain/entities/post.dart';
 import '../../../../core/post/presentation/bloc/PostBloc.dart';
@@ -8,7 +17,9 @@ import '../../../../core/user/data/data_sources/local_database.dart';
 import '../../../../core/user/domain/entities/user.dart';
 import '../../../../core/user/presentation/pages/user_page.dart';
 import '../../../../injection_container.dart';
+import '../../../../shared/notifications/push_notification.dart';
 import '../../../profile/presentation/pages/profile.dart';
+import '../../../share/data/data_sources/share_remote_database.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -26,10 +37,48 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+    //ShareRemoteDatabaseImpl.initializeDynamicLink();
+    FirebaseMessaging.instance.getInitialMessage();
+    FirebaseMessaging.onMessage.listen((event) {
+      LocalNotificationService.display(event);
+    });
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       availablePosts.value = await bloc.retrieve();
     });
     setState(() {});
+  }
+
+  sendNotification(String title, String token)async{
+
+    final data = {
+      'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+      'id': '1',
+      'status': 'done',
+      'message': title,
+    };
+
+    try{
+      http.Response response = await http.post(Uri.parse('https://fcm.googleapis.com/fcm/send'),headers: <String,String>{
+        'Content-Type': 'application/json',
+        'Authorization': 'key=AAAA7NEUBtQ:APA91bFXJb8RWaNPOwdeu3Ih3Jlb3hvQWjp1eRSxMs2xpMuOcJMdzYQYqZHP82-hYCDErFrrQdfrZ9cGiTlkNKvrgJqmaRrNoVqjPKNx34E76tyqwg2GLrt8cR9qSJveJEn0zxyW6qgY'
+      },
+          body: jsonEncode(<String,dynamic>{
+            'notification': <String,dynamic> {'title': title,'body': 'You are followed by someone'},
+            'priority': 'high',
+            'data': data,
+            'to': token
+          })
+      );
+
+
+      if(response.statusCode == 200){
+        print("Yeh notification is sent");
+      }
+
+    }catch(e){
+      print(e);
+    }
+
   }
 
   void logout() async {
@@ -73,56 +122,84 @@ class _HomePageState extends State<HomePage> {
             body: ListView.builder(
                 itemCount: value.length,
                 itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    leading: const CircleAvatar(
-                      radius: 30,
-                      backgroundColor: Colors.yellow,
-                    ),
-                    title: TextButton(
-                      onPressed: () {
-                        if(value[index].author == firebase_auth.FirebaseAuth.instance.currentUser!.uid){
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const UserPage()));
-                        } else{
-                        Navigator.push(
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => Profile(
-                                userID: value[index].author,
-                              )));}
-                      },
-                      child: Text('@${value[index].author}',
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium!
-                              .apply(fontSizeDelta: 1, fontWeightDelta: 1)),
+                              builder: (context) =>
+                                  PostDetails(postId: value[index].id)));
+                    },
+                    child: ListTile(
+                      leading: const CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.yellow,
+                      ),
+                      title: TextButton(
+                        onPressed: () {
+                          if (value[index].author ==
+                              firebase_auth
+                                  .FirebaseAuth.instance.currentUser!.uid) {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const UserPage()));
+                          } else {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Profile(
+                                          userID: value[index].author,
+                                        )));
+                          }
+                        },
+                        child: Text('@${value[index].author}',
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .apply(fontSizeDelta: 1, fontWeightDelta: 1)),
+                      ),
+                      subtitle: Column(
+                        children: [
+                          Text(value[index].postText),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.poll_outlined)),
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.mode_comment_rounded)),
+                              IconButton(
+                                  onPressed: () async {
+                                    print(value[index].author);
+                                    final device = await FirebaseFirestore.instance.collection('devices').doc(value[index].author).get();
+                                    final result = device.data()!;
+                                    final token = Device.fromJson(result);
+                                    print(token.fcmToken);
+                                    await sendNotification('Unfollow', token.fcmToken);
+                                  },
+                                  icon: const Icon(
+                                      Icons.favorite_border_outlined)),
+                              IconButton(
+                                  onPressed: () async {
+                                    print(
+                                        '=======================================');
+                                    final generatedLink =
+                                        await ShareRemoteDatabaseImpl()
+                                            .createDynamicLink(value[index].id);
+                                    await Share.share(generatedLink, subject: 'Check this out');
+                                    print(generatedLink);
+
+                                  },
+                                  icon: const Icon(Icons.share_rounded))
+                            ],
+                          )
+                        ],
+                      ),
+                      isThreeLine: true,
                     ),
-                    subtitle: Column(
-                      children: [
-                        Text(value[index].postText),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.poll_outlined)),
-                            IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.mode_comment_rounded)),
-                            IconButton(
-                                onPressed: () {},
-                                icon:
-                                    const Icon(Icons.favorite_border_outlined)),
-                            IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.share_rounded))
-                          ],
-                        )
-                      ],
-                    ),
-                    isThreeLine: true,
                   );
                 }),
             floatingActionButton: FloatingActionButton(
