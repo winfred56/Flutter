542ac1274414a60910bc0f0d5fa0ecd48037ab56
winfred <winfred242002@gmail.com>
2023-01-26 13:04:37 +0000

    :recycle: refactor

--- google maps/lib/main.dart
+++ google maps/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Google Maps Demo',
-      home: MapSample(),
+      home: const MapSample(),
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
