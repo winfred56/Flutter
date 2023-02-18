edd74506d1deb2b237947d872fbc096a15d5ba3b
winfred <winfred242002@gmail.com>
2023-02-18 20:30:47 +0000

    :bug: initial colors

---
+++ elections_app/lib/shared/theme/color_scheme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+ColorScheme darkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color(0xFF61BC31),
+  primary: const Color(0xFF61BC31),
+  onPrimary: const Color(0xFFFFFFFF),
+  background: const Color(0xFF131313),
+  onBackground: const Color(0xFFFFFFFF),
+  surface: const Color(0xFF262627),
+  onSurface: const Color(0xFF8B8B8B),
+);
+
+ColorScheme lightColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.light,
+  seedColor: const Color(0xFF61BC31),
+  primary: const Color(0xFF61BC31),
+  onPrimary: const Color(0xFFffffff),
+  background: const Color(0xFFffffff),
+  onBackground: const Color(0xFF1a1c18),
+  surface: const Color(0xffF5F5F5),
+  onSurface: const Color(0xFF8B8B8B),
+);
