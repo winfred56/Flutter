cba2a042c1bf619475a6d79fc381bff511dbbb41
winfred <winfred242002@gmail.com>
2023-03-07 13:44:26 +0000

    👨 create electorate feature

--- knust_elect/lib/main.dart
+++ knust_elect/lib/main.dart
@@ -1,71 +1,50 @@
+import 'dart:async';
+
+import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:knust_elect/src/signin/presentation/pages/signin_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'firebase_options.dart';
+import 'shared/data/hive_adapters.dart';
+import 'shared/platform/push_notification.dart';
+import 'shared/routes.dart';
+import 'shared/themes/widget theme.dart';
+import 'core/electorate/data/database/electorate_local_database.dart';
+import 'injection_container.dart' as di;
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+Future<void> main() async {
+  ///  keytool -list -v -keystore ~/.android/debug.keystore -alias androiddebugkey -storepass android -keypass android
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  di.init();
+  await HiveAdapters.setUp();
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  final authState = await di.sl<ElectorateLocalDatabase>().authenticationStatus();
+  await di.sl<PushNotification>().initializeNotification();
+  await runZonedGuarded<Future<void>>(
+        () async {
+      runApp(DevicePreview(
+          enabled: !kReleaseMode,
+          builder: (context) => MaterialApp(
+              debugShowCheckedModeBanner: false,
+              routes: Routes.routes,
+              useInheritedMediaQuery: true,
+              locale: DevicePreview.locale(context),
+              builder: DevicePreview.appBuilder,
+              home: const SignInPage(),
+              title: 'KNUST Elect',
+            theme: lightTheme,
+            darkTheme: darkTheme,
+            themeMode: ThemeMode.system)));
+      FlutterNativeSplash.remove();
+    },
+        (error, stack) =>
+        FirebaseCrashlytics.instance.recordError(error, stack, fatal: true)
+  );
 }
