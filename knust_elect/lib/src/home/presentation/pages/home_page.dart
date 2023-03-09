9c99f7c7d32502e9dfc47cf6683dbb028013c683
winfred <winfred242002@gmail.com>
2023-03-09 15:44:14 +0000

    ♻ Refactor

--- knust_elect/lib/src/home/presentation/pages/home_page.dart
+++ knust_elect/lib/src/home/presentation/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../../../onboarding/presentation/pages/onboarding_page.dart';
 import '../../../../injection_container.dart';
 import '../bloc/home_bloc.dart';
 
@@ -23,6 +24,8 @@ class _HomePageState extends State<HomePage> {
               onPressed: () async {
                 FirebaseAuth.instance.signOut();
                 await bloc.logOUt();
+                if (mounted){}
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> const OnboardingPage()));
               },
               child: const Text('Logout'))),
     );
