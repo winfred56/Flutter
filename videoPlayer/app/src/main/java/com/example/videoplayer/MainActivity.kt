7a4c53a9c92e9d48b24e7bc45629afb643a9e46e
winfred <winfred242002@gmail.com>
2023-09-12 10:42:15 +0000

    :zap: play video

--- videoPlayer/app/src/main/java/com/example/videoplayer/MainActivity.kt
+++ videoPlayer/app/src/main/java/com/example/videoplayer/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.videoplayer
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val videoView = findViewById<VideoView>(R.id.videoView)
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        val uri: Uri = Uri.parse("android.resource://$packageName/${R.raw.introduction}")
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
+
     }
 }
