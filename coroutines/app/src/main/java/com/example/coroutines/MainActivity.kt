43bfcf6bf7b5d3d857a6fdfb023e53b317b65e42
winfred <winfred242002@gmail.com>
2023-09-15 12:43:52 +0000

    :zap: switch coroutine

--- coroutines/app/src/main/java/com/example/coroutines/MainActivity.kt
+++ coroutines/app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -5,9 +5,16 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
+    private lateinit var messageTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,18 +22,24 @@ class MainActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.tvCount)
         val countButton = findViewById<Button>(R.id.btnCount)
         val downloadButton = findViewById<Button>(R.id.btnDownload)
+        messageTextView = findViewById<TextView>(R.id.tvMessage)
 
         countButton.setOnClickListener {
             textView.text = count++.toString()
         }
         downloadButton.setOnClickListener {
-            downloadUserData()
+            CoroutineScope(Dispatchers.IO).launch{
+                downloadUserData()
+            }
+
+        }
+    }
+    private suspend fun downloadUserData() {
+        for (i in 1..500000) {
+            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main){ messageTextView.text = "Downloading user $i in ${Thread.currentThread().name}" }
+            delay(1000)
         }
     }
 }
 
-private fun downloadUserData() {
-    for (i in 1..200000) {
-        Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
-    }
-}
