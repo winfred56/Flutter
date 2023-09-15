5afac9233c75209206b959125b887307ab915c87
winfred <winfred242002@gmail.com>
2023-09-15 10:20:35 +0000

    :zap: coroutine dependency

--- coroutines/app/build.gradle.kts
+++ coroutines/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
