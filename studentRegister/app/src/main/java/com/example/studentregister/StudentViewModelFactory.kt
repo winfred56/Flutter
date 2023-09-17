fb85566f7191fc5996b5741e0ed44c3ef3582d42
winfred <winfred242002@gmail.com>
2023-09-17 12:04:28 +0000

    :zap: student view model

---
+++ studentRegister/app/src/main/java/com/example/studentregister/StudentViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.example.studentregister
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.studentregister.db.StudentDao
+
+class StudentViewModelFactory(private val dao: StudentDao) : ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>):T {
+        if (modelClass.isAssignableFrom(StudentViewModel::class.java)){
+            return StudentViewModel(dao) as T
+        }
+        throw IllegalArgumentException("Unknown View Model class")
+
+    }
+}
