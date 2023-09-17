581a960f04eadb726b67eb49c17f649c852355d4
winfred <winfred242002@gmail.com>
2023-09-17 11:12:55 +0000

    :fire: student entity

---
+++ studentRegister/app/src/main/java/com/example/studentregister/db/Student.kt
@@ -0,0 +1,16 @@
+package com.example.studentregister.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity("student_data_table")
+data class Student(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "student_id")
+    var id: Int,
+    @ColumnInfo(name = "student_name")
+    var name: String,
+    @ColumnInfo(name = "student_email")
+    var email: String
+)
