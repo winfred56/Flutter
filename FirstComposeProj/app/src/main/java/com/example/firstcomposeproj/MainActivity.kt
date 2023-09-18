fac7a78e0ac20b96f83459664c8386619213e860
winfred <winfred242002@gmail.com>
2023-09-18 12:16:31 +0000

    :fire: Columns $ Rows

--- FirstComposeProj/app/src/main/java/com/example/firstcomposeproj/MainActivity.kt
+++ FirstComposeProj/app/src/main/java/com/example/firstcomposeproj/MainActivity.kt
@@ -5,19 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.firstcomposeproj.ui.theme.FirstComposeProjTheme
-import com.example.firstcomposeproj.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,37 +28,70 @@ class MainActivity : ComponentActivity() {
             FirstComposeProjTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column {
-                        Greeting("Android")
-                        Text("Winfred is Awesome", style = Typography.headlineLarge)
-                        CustomItem(text = "Composable @ 1", weight = 1f, width = 400)
-                        CustomItem(text = "Composable @ 2", weight = 2f, width = 300, color = MaterialTheme.colorScheme.error)
-                        CustomItem(text = "Composable @ 3", weight = 1f, width = 200)
+                    Column(
+                        modifier = Modifier.safeContentPadding(),
+                        verticalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Surface(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 8.dp)
+                                .height(100.dp), color = MaterialTheme.colorScheme.primary
+                        ) {
 
+                        }
+                        Surface(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 8.dp)
+                                .height(100.dp), color = MaterialTheme.colorScheme.error
+                        ) {
+
+                        }
+                        Surface(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 8.dp)
+                                .height(100.dp), color = MaterialTheme.colorScheme.primary
+                        ) {
+
+                        }
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .fillMaxWidth()
+                                .padding(top = 8.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Surface(
+                                modifier = Modifier
+                                    .fillMaxHeight()
+                                    .width(125.dp),
+                                color = MaterialTheme.colorScheme.primary
+                            ) {}
+                            Surface(
+                                modifier = Modifier
+                                    .fillMaxHeight()
+                                    .width(125.dp),
+                                color = MaterialTheme.colorScheme.error
+                            ) {}
+                            Surface(
+                                modifier = Modifier
+                                    .fillMaxHeight()
+                                    .width(125.dp),
+                                color = MaterialTheme.colorScheme.primary
+                            ) {}
+                        }
                     }
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun ColumnScope.CustomItem(text: String, weight:Float, width: Int, color: Color = MaterialTheme.colorScheme.onBackground){
-    Surface(modifier = Modifier
-        .weight(weight)
-        .width(width.dp)){
-        Text(text = text, style = Typography.titleLarge.copy(color = color.apply { color.blue }))
     }
 }
 
@@ -64,17 +99,60 @@ fun ColumnScope.CustomItem(text: String, weight:Float, width: Int, color: Color 
 @Composable
 fun GreetingPreview() {
     FirstComposeProjTheme {
-        Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center){
-            CustomItem(text = "Composable @ 1", weight = 1f, width = 400)
-            Surface(modifier = Modifier
-                .weight(2f)
-                .width(300.dp), color = MaterialTheme.colorScheme.secondary) {
-                Text("Column @ 2")
+        Column(
+            modifier = Modifier.safeContentPadding(),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+                    .height(100.dp), color = MaterialTheme.colorScheme.primary
+            ) {
+
             }
-            Surface(modifier = Modifier
-                .weight(2f)
-                .width(200.dp), color = MaterialTheme.colorScheme.error) {
-                Text("Column @ 3")
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+                    .height(100.dp), color = MaterialTheme.colorScheme.error
+            ) {
+
+            }
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+                    .height(100.dp), color = MaterialTheme.colorScheme.primary
+            ) {
+
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+                    .padding(top = 8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .width(125.dp),
+                    color = MaterialTheme.colorScheme.primary
+                ) {}
+                Surface(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .width(125.dp),
+                    color = MaterialTheme.colorScheme.error
+                ) {}
+                Surface(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .width(125.dp),
+                    color = MaterialTheme.colorScheme.primary
+                ) {}
             }
         }
     }
