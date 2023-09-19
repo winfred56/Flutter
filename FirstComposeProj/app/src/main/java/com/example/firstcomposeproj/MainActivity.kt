034e1f5f59245990a55474bf4ac4170d4690aaa1
winfred <winfred242002@gmail.com>
2023-09-19 17:00:37 +0000

    :100: keyboard options

--- FirstComposeProj/app/src/main/java/com/example/firstcomposeproj/MainActivity.kt
+++ FirstComposeProj/app/src/main/java/com/example/firstcomposeproj/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.firstcomposeproj
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,6 +23,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.firstcomposeproj.ui.theme.FirstComposeProjTheme
@@ -33,10 +42,28 @@ class MainActivity : ComponentActivity() {
                     TextField(
                         value = textController,
                         onValueChange = { newText -> textController = newText },
-                        modifier = Modifier.padding(
-                            top = 5.dp
+                        label = { Text("Email") },
+                        modifier = Modifier.padding(top = 5.dp),
+                        leadingIcon = {
+                            Icon(imageVector = Icons.Default.Email, contentDescription = "Email")
+                        },
+                        keyboardOptions = KeyboardOptions(
+                            imeAction = ImeAction.Send,
+                            keyboardType = KeyboardType.Text,
+                            autoCorrect = true
+                        ),
+                        keyboardActions = KeyboardActions(
+                            onSend = {
+                                // Handle the send action
+                                Log.d("KEYBOARD_ACTION", "Send button clicked")
+                            },
+                            onDone = {
+                                // Handle the done action (optional)
+                                Log.d("KEYBOARD_ACTION", "Done button clicked")
+                            }
                         )
                     )
+
                 }
 
             }
