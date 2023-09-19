5d19ebed39d6d4917b9cd33d482b0c420c8ed553
winfred <winfred242002@gmail.com>
2023-09-19 16:10:11 +0000

    :fire: first Textfield

--- FirstComposeProj/app/src/main/java/com/example/firstcomposeproj/MainActivity.kt
+++ FirstComposeProj/app/src/main/java/com/example/firstcomposeproj/MainActivity.kt
@@ -5,159 +5,56 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.firstcomposeproj.ui.theme.FirstComposeProjTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FirstComposeProjTheme{
-                ExpandableCard()
+            FirstComposeProjTheme {
+                Column()
+                {
+                    ExpandableCard()
+                    var textController by remember { mutableStateOf("Type your name") }
+                    TextField(
+                        value = textController,
+                        onValueChange = { newText -> textController = newText },
+                        modifier = Modifier.padding(
+                            top = 5.dp
+                        )
+                    )
+                }
+
             }
-//            FirstComposeProjTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier
-//                        .fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    Column(
-//                        modifier = Modifier.safeContentPadding(),
-//                        verticalArrangement = Arrangement.SpaceBetween
-//                    ) {
-//                        Surface(
-//                            modifier = Modifier
-//                                .fillMaxWidth()
-//                                .padding(top = 8.dp)
-//                                .height(100.dp), color = MaterialTheme.colorScheme.primary
-//                        ) {
-//
-//                        }
-//                        Surface(
-//                            modifier = Modifier
-//                                .fillMaxWidth()
-//                                .padding(top = 8.dp)
-//                                .height(100.dp), color = MaterialTheme.colorScheme.error
-//                        ) {
-//
-//                        }
-//                        Surface(
-//                            modifier = Modifier
-//                                .fillMaxWidth()
-//                                .padding(top = 8.dp)
-//                                .height(100.dp), color = MaterialTheme.colorScheme.primary
-//                        ) {
-//
-//                        }
-//
-//                        Row(
-//                            modifier = Modifier
-//                                .fillMaxHeight()
-//                                .fillMaxWidth()
-//                                .padding(top = 8.dp),
-//                            horizontalArrangement = Arrangement.SpaceBetween
-//                        ) {
-//                            Surface(
-//                                modifier = Modifier
-//                                    .fillMaxHeight()
-//                                    .width(125.dp),
-//                                color = MaterialTheme.colorScheme.primary
-//                            ) {}
-//                            Surface(
-//                                modifier = Modifier
-//                                    .fillMaxHeight()
-//                                    .width(125.dp),
-//                                color = MaterialTheme.colorScheme.error
-//                            ) {}
-//                            Surface(
-//                                modifier = Modifier
-//                                    .fillMaxHeight()
-//                                    .width(125.dp),
-//                                color = MaterialTheme.colorScheme.primary
-//                            ) {}
-//                        }
-//                    }
-//
-//                }
-//            }
         }
 
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    FirstComposeProjTheme {
-        Column(
-            modifier = Modifier.safeContentPadding(),
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-            Surface(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 8.dp)
-                    .height(100.dp), color = MaterialTheme.colorScheme.primary
-            ) {
-
-            }
-            Surface(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 8.dp)
-                    .height(100.dp), color = MaterialTheme.colorScheme.error
-            ) {
-
-            }
-            Surface(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 8.dp)
-                    .height(100.dp), color = MaterialTheme.colorScheme.primary
-            ) {
-
-            }
-
-            Row(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .fillMaxWidth()
-                    .padding(top = 8.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Surface(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .width(125.dp),
-                    color = MaterialTheme.colorScheme.primary
-                ) {}
-                Surface(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .width(125.dp),
-                    color = MaterialTheme.colorScheme.error
-                ) {}
-                Surface(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .width(125.dp),
-                    color = MaterialTheme.colorScheme.primary
-                ) {}
-            }
-        }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        var textController by remember { mutableStateOf("Type your name") }
+        TextField(value = textController, onValueChange = { newText -> textController = newText })
     }
 }
