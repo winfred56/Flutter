59c9e428ba9cb535f5933da47ac678f5e480e742
winfred <winfred242002@gmail.com>
2023-09-21 12:45:23 +0000

    :bug: backgroundIndicator

---
+++ compTest/app/src/main/java/com/example/comptest/CustomComponent.kt
@@ -0,0 +1,68 @@
+package com.example.comptest
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.*
+import androidx.compose.ui.geometry.Offset
+
+@Composable
+fun CustomComponent(
+    canvasSize: Dp = 300.dp,
+    indicatorValue: Int = 0,
+    maxIndicatorValue: Int = 100,
+    backgroundIndicatorColor: Color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
+    backgroundIndicatorStrokeWidth : Float = 100f
+){
+    Column (
+        modifier = Modifier.size(canvasSize).drawBehind {
+            val componentSize = size/1.25f
+            backgroundIndicator(
+                componentSize = componentSize,
+                indicatorColor = backgroundIndicatorColor,
+                indicatorStrokeWidth = backgroundIndicatorStrokeWidth
+            )
+        }
+    ){
+
+    }
+}
+
+fun DrawScope.backgroundIndicator(
+    componentSize: Size,
+    indicatorColor: Color,
+    indicatorStrokeWidth: Float
+){
+    drawArc(
+        size = componentSize,
+        color = indicatorColor,
+        startAngle = 150f,
+        sweepAngle = 240f,
+        useCenter = false,
+        style = Stroke(
+            width = indicatorStrokeWidth,
+            cap = StrokeCap.Round
+        ),
+        topLeft = Offset(
+            x = (size.width - componentSize.width) / 2f,
+            y = (size.height - componentSize.height) / 2f
+        )
+    )
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun PreviewCustomComponent(){
+    CustomComponent()
+}
